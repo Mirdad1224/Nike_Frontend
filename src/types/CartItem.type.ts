f4e76d623749f9835637ae3587badf27a473a2d0
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-20 18:57:40 +0330

    add props type for CartItem component

---
+++ src/types/CartItem.type.ts
@@ -0,0 +1,12 @@
+export type CartItemProps = {
+  item: {
+    id: string;
+    title: string;
+    text: string;
+    img: string;
+    color: string;
+    shadow: string;
+    price: string;
+    cartQuantity: number;
+  };
+};
