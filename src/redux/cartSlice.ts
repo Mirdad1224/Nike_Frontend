469b2ca13b4b28a5c9a4b6e1b00c094dbc0c832b
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-20 18:45:16 +0330

    create a cart slice for management user cart

---
+++ src/redux/cartSlice.ts
@@ -0,0 +1,154 @@
+import { createSlice } from "@reduxjs/toolkit";
+import toast from "react-hot-toast";
+
+type cartItems = {
+  cartQuantity: number;
+  id: string;
+  title: string;
+  text: string;
+  rating: string;
+  btn: string;
+  img: string;
+  price: string;
+  color: string;
+  shadow: string;
+};
+
+type initialStateType = {
+  cartState: boolean;
+  cartItems: cartItems[];
+  cartTotalAmount: number;
+  cartTotalQantity: number;
+};
+
+type stateSelectorType = {
+  cart: {
+    cartState: boolean;
+    cartItems: cartItems[];
+    cartTotalAmount: number;
+    cartTotalQantity: number;
+  };
+};
+
+const initialState: initialStateType = {
+  cartState: false,
+  cartItems: localStorage.getItem("cart")
+    ? JSON.parse(localStorage.getItem("cart") || '[]')
+    : [],
+  cartTotalAmount: 0,
+  cartTotalQantity: 0,
+};
+
+const cartSlice = createSlice({
+  initialState,
+  name: "cart",
+  reducers: {
+    setOpenCart: (state, action) => {
+      state.cartState = action.payload.cartState;
+    },
+    setCloseCart: (state, action) => {
+      state.cartState = action.payload.cartState;
+    },
+    setAddItemToCart: (state, action) => {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1;
+
+        toast.success(`Item QTY Increased`);
+      } else {
+        const temp: cartItems = { ...action.payload, cartQuantity: 1 };
+        state.cartItems.push(temp);
+
+        toast.success(`${action.payload.title} added to Cart`);
+      }
+
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+    },
+
+    setRemoveItemFromCart: (state, action) => {
+      const removeItem = state.cartItems.filter(
+        (item) => item.id !== action.payload.id
+      );
+
+      state.cartItems = removeItem;
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+
+      toast.success(`${action.payload.title} Removed From Cart`);
+    },
+
+    setIncreaseItemQTY: (state, action) => {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1;
+
+        toast.success(`Item QTY Increased`);
+      }
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+    },
+
+    setDecreaseItemQTY: (state, action) => {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+
+      if (state.cartItems[itemIndex].cartQuantity > 1) {
+        state.cartItems[itemIndex].cartQuantity -= 1;
+
+        toast.success(`Item QTY Decreased`);
+      }
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+    },
+
+    setClearCartItems: (state, action) => {
+      state.cartItems = [];
+      toast.success(`Cart Cleared`);
+      localStorage.setItem("cart", JSON.stringify(state.cartItems));
+    },
+
+    setGetTotals: (state, action) => {
+      let { totalAmount, totalQTY } = state.cartItems.reduce(
+        (cartTotal, cartItem) => {
+          const { price, cartQuantity } = cartItem;
+          const totalPrice = +price * cartQuantity;
+
+          cartTotal.totalAmount += totalPrice;
+          cartTotal.totalQTY += cartQuantity;
+
+          return cartTotal;
+        },
+        {
+          totalAmount: 0,
+          totalQTY: 0,
+        }
+      );
+
+      state.cartTotalAmount = totalAmount;
+      state.cartTotalQantity = totalQTY;
+    },
+  },
+});
+
+export const {
+  setOpenCart,
+  setCloseCart,
+  setAddItemToCart,
+  setRemoveItemFromCart,
+  setIncreaseItemQTY,
+  setDecreaseItemQTY,
+  setClearCartItems,
+  setGetTotals,
+} = cartSlice.actions;
+
+export const selectCartState = (state: stateSelectorType) => state.cart.cartState;
+export const selectCartItems = (state: stateSelectorType) => state.cart.cartItems;
+
+export const selectTotalAmount = (state: stateSelectorType) => state.cart.cartTotalAmount;
+export const selectTotalQTY = (state: stateSelectorType) => state.cart.cartTotalQantity;
+
+export default cartSlice.reducer;
