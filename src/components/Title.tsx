27be96d6dc2459a41005e1b8d2c9b4f075e5bcb8
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-21 12:56:29 +0330

    create Title component

---
+++ src/components/Title.tsx
@@ -0,0 +1,18 @@
+import { TitleProps } from "../types/Title.type";
+
+const Title = ({ title }: TitleProps) => {
+  return (
+    <>
+      <div className="grid items-center">
+        <h1
+          className="text-5xl lg:text-4xl md:text-3xl font-bold text-slate-900 filter
+         drop-shadow-lg"
+        >
+          {title}
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Title;
