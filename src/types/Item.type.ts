47c0c3ede7a91ab1ea1e85ab9050352faf5298ff
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-21 12:54:24 +0330

    add props type for Item component

---
+++ src/types/Item.type.ts
@@ -0,0 +1,12 @@
+export type ItemProps = {
+  ifExists: boolean;
+  id: string;
+  color: string;
+  shadow: string;
+  title: string;
+  text: string;
+  img: string;
+  btn: string;
+  rating: string;
+  price: string;
+};
