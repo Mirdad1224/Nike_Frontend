902b9623d8acdde26470caf2a8e0f2d584136a33
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-21 12:58:09 +0330

    create Sales component

---
+++ src/components/Sales.tsx
@@ -0,0 +1,29 @@
+import { SalesProps } from "../types/Sales.type";
+import Item from "./Item";
+import Title from "./Title";
+
+const Sales = ({
+  ifExists = false,
+  endpoint: { title, items },
+}: SalesProps) => {
+  return (
+    <>
+      <div className="nike-container">
+        <Title title={title} />
+        <div
+          className={`grid items-center justify-items-center gap-7 lg:gap-5 mt-7 ${
+            ifExists
+              ? "grid-cols-3 xl:grid-cols-2 sm:grid-cols-1"
+              : "grid-cols-4 xl:grid-cols-3 md:grid-cols-2 sm:grid-cols-1"
+          }`}
+        >
+          {items?.map((item, i) => (
+            <Item {...item} key={i} ifExists={ifExists} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Sales;
