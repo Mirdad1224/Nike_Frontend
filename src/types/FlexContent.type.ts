218301be07a4fd7fb1892f306ae0986ff36eff00
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-23 14:57:29 +0330

    add props type for FlexContent component

---
+++ src/types/FlexContent.type.ts
@@ -0,0 +1,12 @@
+export type FlexContentProps = {
+  ifExists?: boolean;
+
+  endpoint: {
+    title: string;
+    text: string;
+    heading: string;
+    btn: string;
+    img: string;
+    url: string;
+  };
+};
