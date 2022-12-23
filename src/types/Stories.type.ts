f191d50ce1b82f67f7830f13f1431efb48c43d78
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-23 14:59:53 +0330

    add props type for Stories component

---
+++ src/types/Stories.type.ts
@@ -0,0 +1,15 @@
+export type StoriesProps = {
+  story: {
+    title: string;
+    news: {
+      title: string;
+      text: string;
+      img: string;
+      url: string;
+      like: string;
+      time: string;
+      by: string;
+      btn: string;
+    }[];
+  };
+};
