97ad02e071f197b3fceabe7c14fc3017c29021e2
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-25 18:35:48 +0330

    add more functionality to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,23 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {api} from './api'
-import { setupListeners } from "@reduxjs/toolkit/query"
+import { apiSlice } from "./api/apiSlice";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import cartReducer from "./cartSlice";
+import authReducer from "./authSlice";
 
 export const store = configureStore({
-    reducer: {
-        [api.reducerPath]: api.reducer,
-        cart: cartReducer,
-    },
-    middleware: getDefaultMiddleware =>
-        getDefaultMiddleware().concat(api.middleware),
-    devTools: false
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    cart: cartReducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
 });
 
-setupListeners(store.dispatch) 
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>;
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch;
+
+setupListeners(store.dispatch);
