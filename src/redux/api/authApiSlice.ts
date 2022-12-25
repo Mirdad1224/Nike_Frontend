79691e748604d792f735c23d5f625f9f9287dc8f
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-25 18:44:09 +0330

    add api slice for auth

---
+++ src/redux/api/authApiSlice.ts
@@ -0,0 +1,49 @@
+import { apiSlice } from "./apiSlice";
+import { setCredentials } from "../authSlice";
+
+export const authApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth/login",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+    signup: builder.mutation({
+      query: (credentials) => ({
+        url: "/auth/register",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
+    sendLogout: builder.mutation({
+      query: () => ({
+        url: "/auth/logout",
+        method: "POST",
+      }),
+    }),
+    refresh: builder.mutation({
+      query: () => ({
+        url: "/auth/refresh",
+        method: "GET",
+      }),
+      async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+        try {
+          const { data } = await queryFulfilled;
+          const { accessToken } = data;
+          dispatch(setCredentials({ accessToken }));
+        } catch (err) {
+          console.log(err);
+        }
+      },
+    }),
+  }),
+});
+
+export const {
+  useLoginMutation,
+  useSignupMutation,
+  useSendLogoutMutation,
+  useRefreshMutation,
+} = authApiSlice;
