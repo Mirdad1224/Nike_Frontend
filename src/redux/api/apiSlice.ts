c778bfabeb02163edae2da70d2e218ba27275109
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-25 18:35:05 +0330

    refactor apiSlice for manage server state

---
+++ src/redux/api/apiSlice.ts
@@ -0,0 +1,60 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+} from "@reduxjs/toolkit/query";
+import { logOut, setCredentials } from "../authSlice";
+import type { RootState } from "../store";
+
+const baseQuery = fetchBaseQuery({
+  baseUrl: process.env.REACT_APP_BASE_URL,
+  credentials: "include",
+  //* or api.getState()
+  prepareHeaders: (headers, { getState }) => {
+    const token = (getState() as RootState).auth.token;
+
+    if (token) {
+      headers.set("authorization", `Bearer ${token}`);
+    }
+    return headers;
+  },
+});
+
+const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  // console.log(args) // request url, method, body
+  // console.log(api) // signal, dispatch, getState()
+  // console.log(extraOptions) //custom like {shout: true}
+
+  let result = await baseQuery(args, api, extraOptions);
+
+  // If you want, handle other status codes, too
+  if (result?.error?.status === 403) {
+    console.log("sending refresh token");
+
+    // send refresh token to get new access token
+    const refreshResult = await baseQuery("/auth/refresh", api, extraOptions);
+
+    if (refreshResult?.data) {
+      // store the new token
+      api.dispatch(setCredentials({ ...refreshResult.data }));
+
+      // retry original query with new access token
+      result = await baseQuery(args, api, extraOptions);
+    } else {
+      api.dispatch(logOut(null));
+    }
+  }
+
+  return result;
+};
+
+export const apiSlice = createApi({
+  baseQuery: baseQueryWithReauth,
+  tagTypes: ["Products", "User", "Cart", "Favorates"], //*will be used for cached data
+  endpoints: (builder) => ({}),
+});
