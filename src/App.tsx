7b2b1af540db3219ea9bcd6ae1e04b729dd061a4
Mirdad1224 <ali.mirdad@yahoo.com>
2022-12-27 12:52:02 +0330

    refine routing and final App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import "./app.css";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Favorates from "./pages/Favorates";
+import RequireAuth from "./components/RequireAuth";
+import PersistLogin from "./components/PersistLogin";
+import Prefetch from "./components/Prefetch";
+import MainLayout from "./components/MainLayout";
+import Page404 from "./pages/404";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<Prefetch />}>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route element={<PersistLogin />}>
+            <Route element={<RequireAuth />}>
+              <Route path="/favorate" element={<Favorates />} />
+            </Route>
+          </Route>
+        </Route>
+      </Route>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="*" element={<Page404 />} />
+    </Routes>
   );
-}
+};
 
 export default App;
