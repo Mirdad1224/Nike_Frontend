ec2f252433846c0029011d246dff420beb8d2f4a
Mirdad1224 <ali.mirdad@yahoo.com>
2023-01-18 11:53:41 +0330

    edit the root element of the main layout

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -6,12 +6,12 @@ import Navbar from "./Navbar"
 
 const MainLayout = () => {
   return (
-    <>
+    <div className="w-full overflow-x-hidden">
         <Navbar />
         <Cart />
         <Outlet />
         <Footer footerAPI={footerAPI} />
-    </>
+    </div>
   )
 }
 export default MainLayout
